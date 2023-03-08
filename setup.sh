df518429049a7174c7221405a311307c54ae842c
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2023-03-08 11:23:02 +0100

    chore: update scripts

--- setup.sh
+++ setup.sh
@@ -20,17 +20,13 @@ git clone https://github.com/DashieTM/dotFiles.git
 cd dotFiles
 ./setup.sh
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cargo install paru
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+paru -S --skipreview hyprland
+paru -S --skipreview xdg-desktop-portal-hyprland-git
+paru -S --skipreview eww 
+paru -S --skipreview hyprpicker 
+paru -S --skipreview hyprpaper
+paru -S --skipreview rofi-lbonn-wayland-git
+paru -S --skipreview rofi-calc
 
-yay -S hyprland
-yay -S xdg-desktop-portal-hyprland-git
-yay -S eww 
-yay -S hyprpicker 
-yay -S hyprpaper
-
-cargo install ncspot
